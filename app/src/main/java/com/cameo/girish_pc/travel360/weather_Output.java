12a7d9899b8fa1e21042b01cb5a21be233ae57ac
Girish-Mehta <girish.mehta100@gmail.com>
2016-10-01 15:05:17 +0530

    Third Commit

---
+++ app/src/main/java/com/cameo/girish_pc/travel360/weather_Output.java
@@ -0,0 +1,13 @@
+package com.cameo.girish_pc.travel360;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class weather_Output extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather__output);
+    }
+}
