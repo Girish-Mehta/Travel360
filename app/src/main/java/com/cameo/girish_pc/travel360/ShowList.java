1e1747ef5f1ef088f4930ab61d08c92ba3b8108c
Girish-Mehta <girish.mehta100@gmail.com>
2016-10-10 19:44:56 +0530

    Made grid view and linear view possible

--- app/src/main/java/com/cameo/girish_pc/travel360/ShowList.java
+++ app/src/main/java/com/cameo/girish_pc/travel360/ShowList.java
@@ -3,6 +3,7 @@ package com.cameo.girish_pc.travel360;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -19,29 +20,30 @@ public class ShowList extends AppCompatActivity {
         ArrayList<String> words = new ArrayList<String>();
 
         words.add("One degree");
-        words.add("Two degree");
-        words.add("Three degree");
-        words.add("Four degree");
-        words.add("Five degree");
-        words.add("Six degree");
-        words.add("Seven degree");
-        words.add("Eight degree");
-        words.add("Nine degree");
-        words.add("Ten degree");
         words.add("One degree");
         words.add("Two degree");
+        words.add("Two degree");
+        words.add("Three degree");
         words.add("Three degree");
         words.add("Four degree");
+        words.add("Four degree");
+        words.add("Five degree");
         words.add("Five degree");
         words.add("Six degree");
+        words.add("Six degree");
+        words.add("Seven degree");
         words.add("Seven degree");
         words.add("Eight degree");
+        words.add("Eight degree");
+        words.add("Nine degree");
         words.add("Nine degree");
         words.add("Ten degree");
+        words.add("Ten degree");
+
 
         ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
 
-        ListView listView = (ListView) findViewById(R.id.list);
+        GridView listView = (GridView) findViewById(R.id.list);
 
         listView.setAdapter(itemsAdapter);
     }
